7c6c16f7779fa838f6a091844d8eb0139576c7dc
Joshua Clayton <joshua.clayton@gmail.com>
2016-10-13 06:21:59 -0400

    Support box-and-whisker graphs

--- app/lib/chart.js
+++ app/lib/chart.js
@@ -1,5 +1,7 @@
 $ = require('jquery');
-require('highcharts');
+const Highcharts = require('highcharts');
+require('highcharts/highcharts-more')(Highcharts);
+require('highcharts/modules/exporting')(Highcharts);
 window.moment = require('moment');
 
 // const START_DATE = moment("01-22-2015", "MM-DD-YYYY");
@@ -17,6 +19,8 @@ const typeToHighchartsRenderedType = function(type) {
       return "column";
     case "stacked-bar":
       return "column";
+    case "box-plot":
+      return "boxplot";
   }
 };
 
@@ -65,8 +69,21 @@ const generateSeriesFromData = function(startDate, dateInterval, data) {
   return data.graphs.map((graph, idx) => {
     if (graph._type === "scatter-plot") {
       const res = scatterPlotDataToGroups(graph.points);
-      console.log(res);
-      return res
+      return res;
+    } else if (graph._type === "box-plot") {
+      const points = graph.points.map((v, i) => {
+        let _v;
+        if (v.length === 0) {
+          _v = [0,0,0,0,0];
+        }
+
+        return [dateForIndex(startDate, dateInterval, i), ...(_v || v)];
+      });
+      return {
+        type: typeToHighchartsRenderedType(graph._type),
+        data: points,
+        name: graph.name,
+      };
     } else {
       if ($.isArray(graph.points[0])) {
         return graph.points.map(points => {
@@ -113,7 +130,6 @@ const generateTooltip = function(data) {
   if (doesContainScatter(data)) {
     return {
       formatter() {
-        console.log(this);
         const amount = this.point.y;
         const days = this.point.x;
         const category = this.point.series.name;
@@ -137,7 +153,6 @@ class Chart {
   }
 
   renderData(data) {
-    console.log('data', data);
     $(this.selector).highcharts({
       title: { text: null },
       xAxis: generateXAxis(data),
